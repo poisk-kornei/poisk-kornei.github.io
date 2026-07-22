00125f81f1eadcc05d3975403b7fb06f2864dfcd
poisk-kornei <iijii@mail.ru>
2026-07-22 13:37:06 +0300

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,5 @@
+        <!-- Переключатель языков -->
+        <div style="display: flex; justify-content: flex-end; gap: 10px; margin-bottom: 20px;">
+            <button style="background: rgba(255,255,255,0.05); border: 2px solid #ff33b8; color: #fff; padding: 6px 14px; border-radius: 50px; cursor: pointer; font-weight: bold;" onclick="changeLang('ru')">RU</button>
+            <button style="background: rgba(255,255,255,0.05); border: 2px solid rgba(79, 51, 255, 0.3); color: #bfaeff; padding: 6px 14px; border-radius: 50px; cursor: pointer; font-weight: bold;" onclick="changeLang('en')">EN</button>
+        </div>
